af2ee6548bb18e8f34cf76b912fd5732fc376b46
小傅Fox <xfox@cotr.me>
2015-02-10 21:15:12 +0800

    callback

--- js/init.js
+++ js/init.js
@@ -7,3 +7,18 @@ $(document).ready(function() {
   });
   $.material.init();
 });
+
+function Request(argname) //获取get的值
+  {
+    var url = document.location.href;
+    var arrStr = url.substring(url.indexOf("?") + 1).split("&");
+    //return arrStr;
+    for (var i = 0; i < arrStr.length; i++) {
+      var loc = arrStr[i].indexOf(argname + "=");
+      if (loc != -1) {
+        return arrStr[i].replace(argname + "=", "").replace("?", "");
+        break;
+      }
+    }
+    return "";
+  }
